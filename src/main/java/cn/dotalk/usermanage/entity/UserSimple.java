0fce208f1f89fe30b82b68359ae0af87ba3f8b5d
Yongze.chen <sapphire.php@gmail.com>
2021-07-14 18:05:25 +0800

    lombok

--- src/main/java/cn/dotalk/usermanage/entity/UserSimple.java
+++ src/main/java/cn/dotalk/usermanage/entity/UserSimple.java
@@ -1,11 +1,13 @@
 package cn.dotalk.usermanage.entity;
 
 import java.io.Serializable;
+import lombok.Data;
 
 /**
  * user_simple
  * @author 
  */
+@Data
 public class UserSimple implements Serializable {
     private Integer id;
 
@@ -16,36 +18,4 @@ public class UserSimple implements Serializable {
     private String realname;
 
     private static final long serialVersionUID = 1L;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRealname() {
-        return realname;
-    }
-
-    public void setRealname(String realname) {
-        this.realname = realname;
-    }
 }
