beda2be1433eb0abdb7d616bd87f85b66538dbb0
Yongze.chen <sapphire.php@gmail.com>
2021-07-14 18:02:31 +0800

    user

--- src/main/java/cn/dotalk/usermanage/controller/UserController.java
+++ src/main/java/cn/dotalk/usermanage/controller/UserController.java
@@ -3,13 +3,11 @@ package cn.dotalk.usermanage.controller;
 import cn.dotalk.usermanage.entity.UserSimple;
 import cn.dotalk.usermanage.mapper.UserSimpleDao;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.annotation.Resource;
-import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/user")
@@ -18,20 +16,33 @@ public class UserController {
     @Resource
     UserSimpleDao userSimpleDao;
 
-    @RequestMapping("/get")
-    public String get(){
-        UserSimple userSimple = userSimpleDao.selectByPrimaryKey(3);
+    @ResponseBody
+    @RequestMapping(value = "/get/{id}",method = RequestMethod.GET,produces = "application/json;charset=UTF-8")
+    public String get(@PathVariable("id") int id){
+        UserSimple userSimple = userSimpleDao.selectByPrimaryKey(id);
+        System.out.println(userSimple.toString());
         return userSimple.getUsername();
     }
 
-    @RequestMapping("/add")
-    public String add(){
+
+
+    @ResponseBody
+    @RequestMapping(value = "/body/data", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public String writeByBody() {
+        System.out.println(userSimpleDao);
+        return "writeByBody";
+    }
+
+
+
+
+    @RequestMapping(value = "/add")
+    public int add(){
         UserSimple user = new UserSimple();
         user.setRealname("ddd");
         user.setUsername("yongze2");
         user.setPassword("ddddd");
-        userSimpleDao.insert(user);
-        return "add";
+        return userSimpleDao.insert(user);
     }
 
 //    删除
