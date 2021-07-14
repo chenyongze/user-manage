8d243517d1076b8c3e03340784241fd84141ef21
Yongze.chen <sapphire.php@gmail.com>
2021-07-14 12:42:25 +0800

    配置

--- src/main/java/cn/dotalk/usermanage/UserManageApplication.java
+++ src/main/java/cn/dotalk/usermanage/UserManageApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+// 忽略数据库连接
+//@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class UserManageApplication {
 
     public static void main(String[] args) {
